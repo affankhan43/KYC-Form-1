46d40cb2342a22155ceea547120352f73bde2556
affankhan43 <affankhan627@gmail.com>
2018-03-29 20:00:33 +0500

    'kyc'

--- core/funcs.php
+++ core/funcs.php
@@ -11,4 +11,33 @@ function check_code($token){
   }
   return false;
 }
+
+function build_data_files($boundary, $fields, $files){
+    $data = '';
+    $eol = "\r\n";
+
+    $delimiter = '-------------' . $boundary;
+
+    foreach ($fields as $name => $content) {
+        $data .= "--" . $delimiter . $eol
+            . 'Content-Disposition: form-data; name="' . $name . "\"".$eol.$eol
+            . $content . $eol;
+    }
+
+
+    foreach ($files as $name => $content) {
+        $data .= "--" . $delimiter . $eol
+            . 'Content-Disposition: form-data; name="' . $name . '"; filename="' . $name . '"' . $eol
+            //. 'Content-Type: image/png'.$eol
+            . 'Content-Transfer-Encoding: binary'.$eol
+            ;
+
+        $data .= $eol;
+        $data .= $content . $eol;
+    }
+    $data .= "--" . $delimiter . "--".$eol;
+
+
+    return $data;
+}
 ?>
