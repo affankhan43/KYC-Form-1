68d28124c25d9c383f794b4682fb2e58b82c91e2
affankhan43 <affankhan627@gmail.com>
2018-08-29 02:37:56 +0500

    style

--- index.php
+++ index.php
@@ -58,30 +58,35 @@ if(isset($_GET['token']) && isset($_GET['userid']) && isset($_GET['username']) &
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Pixiubit | KYC</title>
-    <style type="text/css">
-    	.kyc-form{
-    		margin: 0 auto;
-    		overflow: hidden;
-    		padding: 10px 0;
-    		align-items: center;
-    		justify-content: space-around;
-    		float: none;
-    	}
-    	.form-group>label{
-    font-weight: bold;
-    font-family:  sans-serif;
-    	}
-    </style>
-  </head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<title>Alladin | KYC</title>
+	<style type="text/css">
+		body{
+			font-family: 'Roboto', sans-serif;
+		}
+		.kyc-form{
+			margin: 0 auto;
+			overflow: hidden;
+			padding: 10px 0;
+			align-items: center;
+			justify-content: space-around;
+			float: none;
+		}
+		.form-group>label{
+			font-weight: bold;
+		}
+		.border-line{
+			border-right: solid 2px #c8c8c8;
+		}
+	</style>
+</head>
   <body>
   	<div class="container">
   		<div class="col-md-12 kyc-form">
