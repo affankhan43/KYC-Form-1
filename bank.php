ff2b7a56281963e5d908bcf63b44d00df287a75b
affankhan43 <affankhan627@gmail.com>
2018-09-02 01:14:30 +0500

    bank_form

--- bank.php
+++ bank.php
@@ -7,18 +7,14 @@ if(isset($_GET['token']) && isset($_GET['userid']) && isset($_GET['username']) &
 	if(!empty($_GET['token']) && !empty($_GET['userid']) && !empty($_GET['username']) && !empty($_GET['bind'])){
 		session_start();
 		$error = array();
-		if(isset($_POST['kyc-submit']) && check_code($_POST['xss_code'])){
-			if(!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['street-line-1']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['country']) && !empty($_POST['phone']) && !empty($_POST['zip']) && !empty($_POST['passport_no']) && !empty($_POST['passport_exp']) && !empty($_POST['passport_isu'])){
-				$address = $_POST['street-line-1']." ,".$_POST['street-line-2'];
+		if(isset($_POST['bnk-submit']) && check_code($_POST['xss_code'])){
+			if(!empty($_POST['iban'])){
 				$auth = "Bearer ".base64_decode($_GET['token']);
-
 				/*-- POST --*/
-				$fields = array('broker_id' => $_GET['bind'], 'userid'=>$_GET['userid'],'username'=>$_GET['username'],'first_name'=>$_POST['first_name'],'last_name'=>$_POST['last_name'],'address'=>$address,'country_of_residence'=>$_POST['country'],'city'=>$_POST['city'],'state'=>$_POST['state'],'phone'=>$_POST['phone'],'postal_code'=>$_POST['zip'],'nationality'=>$_POST['nationality'],'passport_no'=>$_POST['passport_no'],'passport_exp'=>$_POST['passport_exp'],'passport_isu'=>$_POST['passport_isu'],'employment'=>$_POST['employment']);
-				$filenames = array($_FILES['passports']['tmp_name'],$_FILES['passport_selfie']['tmp_name'],$_FILES['statement_bill']['tmp_name']);
-				$files['passport'] = file_get_contents($_FILES['passports']['tmp_name']);
-				$files['passport_selfie'] = file_get_contents($_FILES['passport_selfie']['tmp_name']);
-				$files['statments'] = file_get_contents($_FILES['statement_bill']['tmp_name']);
-				$url = "https://sys.pixiubit.com/api/kyc_form";
+				$fields = array('broker_id' => $_GET['bind'], 'userid'=>$_GET['userid'],'username'=>$_GET['username'],'iban'=>$_POST['iban']);
+				$filenames = array($_FILES['bank_statement']['tmp_name']);
+				$files['statements'] = file_get_contents($_FILES['bank_statement']['tmp_name']);
+				$url = "https://sys.pixiubit.com/api/kyc_bank_form";
 				$curl = curl_init();
 				$boundary = uniqid();
 				$delimiter = '-------------' . $boundary;
@@ -123,9 +119,10 @@ if(isset($_GET['token']) && isset($_GET['userid']) && isset($_GET['username']) &
 					<div class="input-group-md">
 						<label for="iban">Submit Bank Account Statement</label>
 						<input class="form-control" type="file"  accept="image/*, application/pdf" name="bank_statement" />
+						<input type="hidden" class="form-control" name="xss_code" value=<?php echo xss_code_generate(); ?> readonly required>
 					</div>
 					<div class="input-group-md">
-						<input type="submit" class="form-control btn btn-primary" name="kyc-submit" value="Submit">
+						<input type="submit" class="form-control btn btn-primary" name="bnk-submit" value="Submit">
 					</div>
 				</div>
 			</form>
