04d2649b7f592d8444612e36496cf4f51f9adb37
affankhan43 <affankhan627@gmail.com>
2018-09-28 16:54:07 +0500

    kyc

--- bank.php
+++ bank.php
@@ -6,53 +6,97 @@
 if(isset($_GET['token']) && isset($_GET['userid']) && isset($_GET['username']) && isset($_GET['bind'])){
 	if(!empty($_GET['token']) && !empty($_GET['userid']) && !empty($_GET['username']) && !empty($_GET['bind'])){
 		session_start();
-		$error = array();
 		if(isset($_POST['bnk-submit']) && check_code($_POST['xss_code'])){
 			if(!empty($_POST['iban'])){
-				$auth = "Bearer ".base64_decode($_GET['token']);
-				/*-- POST --*/
-				$fields = array('broker_id' => $_GET['bind'], 'userid'=>$_GET['userid'],'username'=>$_GET['username'],'iban'=>$_POST['iban']);
-				$filenames = array($_FILES['bank_statement']['tmp_name']);
-				$files['statements'] = file_get_contents($_FILES['bank_statement']['tmp_name']);
-				$url = "https://sys.pixiubit.com/api/kyc_bank_form";
-				$curl = curl_init();
-				$boundary = uniqid();
-				$delimiter = '-------------' . $boundary;
-				$post_data = build_data_files($boundary, $fields, $files);
-				curl_setopt_array($curl, array(
-					CURLOPT_URL => $url,
-					CURLOPT_RETURNTRANSFER => 1,
-					CURLOPT_MAXREDIRS => 10,
-					CURLOPT_TIMEOUT => 30,
-					CURLOPT_CUSTOMREQUEST => "POST",
-					CURLOPT_POST => 1,
-					CURLOPT_POSTFIELDS => $post_data,
-					CURLOPT_HTTPHEADER => array(
-						"Authorization: ".$auth."",
-						"Content-Type: multipart/form-data; boundary=" . $delimiter,
-						"Content-Length: " . strlen($post_data)),
-						"Accept: application/json"
-				));
-				$response = curl_exec($curl);
-				$err = curl_error($curl);
-				curl_close($curl);
-				if($err){
-					$error[1] = "cURL Error #:" . $err;
-				}
-				else{
-					$data = json_decode($response);
-					if($data->success == false){
-						$error[2] = $data->error;
+				$allowed =  array('png','jpg','PNG','JPG','JPEG','jpeg');
+				$filenames = array($_FILES['bank_statement']['name']);
+				$ext = array(pathinfo($filenames[0], PATHINFO_EXTENSION));
+				if($_FILES['bank_statement']['error']==0){
+					if(in_array($ext[0], $allowed)){
+						if($_FILES['bank_statement']['size'] < 1572864 && $_FILES['bank_statement']['size'] != 0){
+							$auth = "Bearer ".base64_decode($_GET['token']);
+							/*-- POST --*/
+							$fields = array('broker_id' => $_GET['bind'], 'userid'=>$_GET['userid'],'username'=>$_GET['username'],'iban'=>$_POST['iban']);
+							$files['statements'] = file_get_contents($_FILES['bank_statement']['tmp_name']);
+							$url = "https://sys.pixiubit.com/api/kyc_bank_form";
+							$curl = curl_init();
+							$boundary = uniqid();
+							$delimiter = '-------------' . $boundary;
+							$post_data = build_data_files($boundary, $fields, $files);
+							curl_setopt_array($curl, array(
+								CURLOPT_URL => $url,
+								CURLOPT_RETURNTRANSFER => 1,
+								CURLOPT_MAXREDIRS => 10,
+								CURLOPT_TIMEOUT => 30,
+								CURLOPT_CUSTOMREQUEST => "POST",
+								CURLOPT_POST => 1,
+								CURLOPT_POSTFIELDS => $post_data,
+								CURLOPT_HTTPHEADER => array(
+									"Authorization: ".$auth."",
+									"X-Requested-With: XMLHttpRequest",
+									"Content-Type: multipart/form-data; boundary=" . $delimiter,
+									"Content-Length: " . strlen($post_data))
+							));
+							$response = curl_exec($curl);
+							$err = curl_error($curl);
+							curl_close($curl);
+							if($err){
+								$error[1] = "cURL Error #:" . $err;
+							}
+							else{
+								$data = json_decode($response);
+								//echo json_encode($response);
+								if($data->success == false){
+									$error[2] = $data->error;
+								}
+								else{
+									$success[0] = $data->message;
+								}
+							}
+						}
+						else{
+							$error[0] = "FILE SIZE MUST BE LESS THAN 1.5 MB";
+						}
 					}
 					else{
-						$success[0] = $data->message;
+						$error[0] = "DOCUMENTS ONLY ACCEPTED IN JPG, JPEG, PNG FORMAT";
 					}
+				}
+				else{
+					$error[0] = "ERROR IN UPLOADING".json_encode($_FILES['bank_statement']['error']);
 				}
 			}
 			else{
 				$error[0] = "Fill All Fields";
 			}
 		}
+		$user_check = new Curl();
+		$user_check->setHeader('Authorization','Bearer '.base64_decode($_GET['token']));
+		$user_check->setHeader('X-Requested-With', 'XMLHttpRequest');
+		$user_check->post('https://sys.pixiubit.com/api/user_check',array(
+        	'broker_id'=>$_GET['bind'],
+			'userid'=>$_GET['userid'],
+			'username'=>$_GET['username'],
+			'message'=>'kyc-level-2'
+        ));
+        if ($user_check->error) {
+          $check_msg[0] = "Authentication Failed";
+        }
+        else{
+        	$checker = $user_check->response;
+        	if(isset($checker->success)){
+        		if($checker->success == true){
+        			$form_avail = true;
+        		}
+        		else{
+        			$check_msg[0] = $checker->message;
+        		}
+        	}
+        	else{
+        		$check_msg[0] = "Under Maintenance.";
+        	}
+        }
+		
 ?>
 <!doctype html>
 <html lang="en">
@@ -92,49 +136,60 @@ if(isset($_GET['token']) && isset($_GET['userid']) && isset($_GET['username']) &
   <body>
   	<div class="container">
   		<div class="col-md-12 kyc-form">
-  			<?php if(isset($error[0])){ ?>
-  			<div class="alert alert-danger" role="alert">
-  				<strong>Oh snap!</strong> <?php echo $error[0]; ?>
-			</div>
-			<?php }elseif(isset($error[1])){ ?>
-  			<div class="alert alert-warning" role="alert">
-  				<strong>Oh snap!</strong> <?php echo $error[1]; ?>
-			</div>
-			<?php }elseif(isset($error[2])){ ?>
-  			<div class="alert alert-success" role="alert">
-  				<strong> <?php echo $error[2]; ?> </strong>
-			</div>
-			<?php }elseif(isset($success[0])){ ?>
-			<div class="alert alert-success" role="alert">
-				<strong> <?php echo $success[0]; ?> </strong>
-			</div>
-			<?php }if(!isset($success[0])){ ?>	
-			<form class="row bank_form" method="post" enctype="multipart/form-data">
-				<div class="form-group">
-					<div class="input-group-md">
-						<label for="iban">Bank Account IBAN</label>
-						<input class="form-control" placeholder="IBAN NUMBER" type="text" name="iban" />
+  			<?php if((isset($form_avail) && $form_avail==true) || isset($error[0]) || isset($error[1]) || isset($error[2]) || isset($success[0])){
+  					if(isset($error[0])){ ?>
+  						<div class="alert alert-danger" role="alert">
+  							 <?php echo $error[0]; ?>
+  						</div>
+  					<?php }elseif(isset($error[1])){ ?>
+  						<div class="alert alert-warning" role="alert">
+  							 <?php echo $error[1]; ?>
+  						</div>
+  					<?php }elseif(isset($error[2])){ ?>
+  						<div class="alert alert-success" role="alert">
+  							<strong> <?php echo $error[2]; ?> </strong>
+  						</div>
+  					<?php }if(isset($success[0])){ ?>
+  						<div class="alert alert-success" role="alert">
+  							<strong> <?php echo $success[0]; ?> </strong>
+  						</div>
+  					<?php }else{?>
+						<form class="row bank_form" method="post" enctype="multipart/form-data">
+							<div class="form-group">
+								<div class="input-group-md">
+									<label for="iban">Bank Account IBAN</label>
+									<input class="form-control" placeholder="IBAN NUMBER" type="text" name="iban" />
+								</div>
+								<br/>
+								<div class="input-group-md">
+									<label for="iban">Submit Bank Account Statement</label>
+									<input class="form-control" type="file"  accept="image/*, application/pdf" name="bank_statement" />
+									<input type="hidden" class="form-control" name="xss_code" value=<?php echo xss_code_generate(); ?> readonly required>
+								</div>
+								<br>
+								<div class="input-group-md">
+									<input type="submit" class="form-control btn btn-primary" name="bnk-submit" value="Submit">
+								</div>
+								<br>
+								<div class="alert alert-danger">
+									ONLY <strong>SEPA</strong> REGION BANKING SUPPORTED
+								</div>
+								<div class="alert alert-danger">
+									BANKING STATEMENT MUST NOT BE OLDER THAN <strong>90 DAYS</strong>
+								</div>
+							</div>
+						</form>
+					<?php } ?>
+				<?php }elseif(isset($check_msg[0])){ ?>
+					<div class="alert alert-warning" role="alert">
+						<?php echo $check_msg[0]; ?>
 					</div>
-					<br/>
-					<div class="input-group-md">
-						<label for="iban">Submit Bank Account Statement</label>
-						<input class="form-control" type="file"  accept="image/*, application/pdf" name="bank_statement" />
-						<input type="hidden" class="form-control" name="xss_code" value=<?php echo xss_code_generate(); ?> readonly required>
+				<?php }else{ ?>
+					<div class="alert alert-warning" role="alert">
+						<?php echo "Under Maintenance.."; ?>
 					</div>
-					<br>
-					<div class="input-group-md">
-						<input type="submit" class="form-control btn btn-primary" name="bnk-submit" value="Submit">
-					</div>
-					<br>
-					<div class="alert alert-danger">
-						ONLY <strong>SEPA</strong> REGION BANKING SUPPORTED
-					</div>
-					<div class="alert alert-danger">
-						BANKING STATEMENT MUST NOT BE OLDER THAN <strong>90 DAYS</strong>
-					</div>
-				</div>
-			</form>
-			<?php } ?>
+				<?php } ?>
+
 		</div>
 	</div>
 
